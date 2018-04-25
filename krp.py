483f56c5dfe1f4725bf7c3da7e182b3eabf3b636
nielsbosma <niels@bosmainteractive.se>
2018-04-25 22:36:31 +0200

    Testing out blinking.

--- krp.py
+++ krp.py
@@ -1,17 +1,32 @@
 import Adafruit_CharLCD as LCD
+import RPi.GPIO as GPIO
+import time
 
-lcd_rs = 25
-lcd_en = 24
-lcd_d4 = 23
-lcd_d5 = 17
-lcd_d6 = 18
-lcd_d7 = 22
-lcd_backlight = 4
-lcd_columns = 16
-lcd_rows = 2
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
 
-lcd = LCD.Adafruit_CharLCD(lcd_rs, lcd_en, lcd_d4, lcd_d5, lcd_d6, lcd_d7, lcd_columns, lcd_rows, lcd_backlight)
+# lcd_rs = 25
+# lcd_en = 24
+# lcd_d4 = 23
+# lcd_d5 = 17
+# lcd_d6 = 18
+# lcd_d7 = 22
+# lcd_backlight = 4
+# lcd_columns = 16
+# lcd_rows = 2
+#
+# lcd = LCD.Adafruit_CharLCD(lcd_rs, lcd_en, lcd_d4, lcd_d5, lcd_d6, lcd_d7, lcd_columns, lcd_rows, lcd_backlight)
+#
+# lcd.home()
+# lcd.clear()
+# lcd.message("HelloWorld!")
 
-lcd.home()
-lcd.clear()
-lcd.message("HelloWorld!")
+status_led = 21
+
+GPIO.setup(status_led, GPIO.OUT)
+
+while True:
+    GPIO.output(status_led, GPIO.HIGH)
+    time.sleep(2)
+    GPIO.output(status_led, GPIO.LOW)
+    time.sleep(2)
