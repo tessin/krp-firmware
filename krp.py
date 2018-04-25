6a8c57ad0150e07dc35969c113b8932b8136a7a2
nielsbosma <niels@bosmainteractive.se>
2018-04-25 21:41:39 +0200

    Lcd tests.

---
+++ krp.py
@@ -0,0 +1,17 @@
+import Adafruit_CharLCD as LCD
+
+lcd_rs = 25
+lcd_en = 24
+lcd_d4 = 23
+lcd_d5 = 17
+lcd_d6 = 18
+lcd_d7 = 22
+lcd_backlight = 4
+lcd_columns = 16
+lcd_rows = 2
+
+lcd = LCD.Adafruit_CharLCD(lcd_rs, lcd_en, lcd_d4, lcd_d5, lcd_d6, lcd_d7, lcd_columns, lcd_rows, lcd_backlight)
+
+lcd.home()
+lcd.clear()
+lcd.message("HelloWorld!")
