67b41f624eaec33653fde4d2087379a78407c4be
nielsbosma <niels@bosmainteractive.se>
2018-11-10 13:02:25 +0100

    Work

---
+++ krp-engine.py
@@ -0,0 +1,96 @@
+import requests
+import socket
+
+#################################################################
+
+class KrpController:
+
+    baseUrl = "https://azfn-linqpad-web-app-e5opllzphjvwc.azurewebsites.net/api/krp"
+    minimumMinutesBetweenLogs = 0
+    users = []
+    currentUser = 0;
+
+    def __init__(self, client):
+        self.client = client
+        self.client.controller = self;
+
+    def init(self):
+        self.client.init()
+        self.client.write(self.client.getIp(),"Loading...")
+        url = f"{self.baseUrl}/config"
+        response = requests.get(url=url)
+        data = response.json()
+        self.minimumMinutesBetweenLogs = data['MinimumMinutesBetweenLogs']
+        for jUser in data['Users']:
+            self.users.append(KrpPlayer(jUser['Id'], jUser['FirstName'], jUser['LastName']))
+        self.client.clear()
+        self.client.loop()
+
+    def onLeft(self):
+        self.client.write("left","")
+
+    def onRight(self):
+        self.client.write("right", "")
+
+    def onOk(self):
+        self.client.write("ok", "")
+
+#################################################################
+
+class KrpPlayer:
+
+    def __init__(self, id, firstName, lastName):
+        self.id = id,
+        firstName = firstName,
+        lastName = lastName
+
+#################################################################
+
+class KrpClient:
+    controller = None
+
+    def init(self):
+        pass
+
+    def write(self,top16,bottom16):
+        pass
+
+    def clear(self):
+        self.write("","")
+
+    def loop(self):
+        pass
+
+    def getIp(self):
+        return socket.gethostbyname(socket.gethostname())
+
+class KrpConsoleClient(KrpClient):
+
+    def init(self):
+        pass
+
+    def write(self, top16, bottom16):
+        top16 = '{0: <16}'.format(top16[:16])
+        bottom16 = '{0: <16}'.format(bottom16[:16])
+        print("+----------------+")
+        print("|"+top16+"|")
+        print("+----------------+")
+        print("|"+bottom16+"|")
+        print("+----------------+")
+
+    def loop(self):
+        while True:
+            key = input("")
+            if key == "r":
+                controller.onRight()
+            if key == "l":
+                controller.onLeft()
+            if key == "o":
+                controller.onOk()
+            if key == "q":
+                break
+
+#################################################################
+
+controller = KrpController(KrpConsoleClient())
+controller.init()
--- krp.py
+++ krp.py
@@ -1,26 +1,26 @@
 import Adafruit_CharLCD as LCD
 import RPi.GPIO as GPIO
-import time
+from time import sleep
 
 GPIO.cleanup()
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(True)
 
-# lcd_rs = 25
-# lcd_en = 24
-# lcd_d4 = 23
-# lcd_d5 = 17
-# lcd_d6 = 18
-# lcd_d7 = 22
-# lcd_backlight = 4
-# lcd_columns = 16
-# lcd_rows = 2
-#
-# lcd = LCD.Adafruit_CharLCD(lcd_rs, lcd_en, lcd_d4, lcd_d5, lcd_d6, lcd_d7, lcd_columns, lcd_rows, lcd_backlight)
-#
-# lcd.home()
-# lcd.clear()
-# lcd.message("HelloWorld!")
+lcd_rs = 25
+lcd_en = 24
+lcd_d4 = 23
+lcd_d5 = 17
+lcd_d6 = 18
+lcd_d7 = 22
+lcd_backlight = 4
+lcd_columns = 16
+lcd_rows = 2
+
+lcd = LCD.Adafruit_CharLCD(lcd_rs, lcd_en, lcd_d4, lcd_d5, lcd_d6, lcd_d7, lcd_columns, lcd_rows, lcd_backlight)
+
+lcd.home()
+lcd.clear()
+lcd.message("HelloWorld!")
 
 led_pin = 20
 GPIO.setup(led_pin, GPIO.OUT)
@@ -41,11 +41,26 @@ GPIO.setup(right_button_pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 #     GPIO.output(led_pin, GPIO.LOW)
 #     time.sleep(2)
 
-while True:
-    input_state = GPIO.input(ok_button_pin)
-    if input_state == False:
-        print('OK Button Pressed')
-        time.sleep(0.2)
+GPIO.output(led_pin, GPIO.HIGH)
+
+#while True:
+#    input_state = GPIO.input(ok_button_pin)
+#    if input_state == False:
+#        print('OK Button Pressed')
+#        time.sleep(0.2)
+
+def buttonPressed(button):
+    print(button)
+
+GPIO.add_event_detect(left_button_pin, GPIO.FALLING, callback=lambda x: buttonPressed("left"), bouncetime=2000)
+GPIO.add_event_detect(ok_button_pin, GPIO.FALLING, callback=lambda x: buttonPressed("ok"), bouncetime=2000)
+GPIO.add_event_detect(right_button_pin, GPIO.FALLING, callback=lambda x: buttonPressed("right"), bouncetime=2000)
+
+try:
+    while True:
+        sleep(1)
+except KeyboardInterrupt:
+    GPIO.cleanup()
 
 
 
