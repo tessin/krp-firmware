0a2bd7d5016712b0b1a4664aa3f0043e2014f794
Superman <superman@Supermans-iMac.local>
2019-04-10 00:29:41 +1000

    remove socket

--- krp.py
+++ krp.py
@@ -3,7 +3,6 @@ import RPi.GPIO as GPIO
 from time import sleep
 import commands
 from threading import Thread
-from socket import *
 
 GPIO.cleanup()
 GPIO.setmode(GPIO.BCM)
@@ -59,15 +58,162 @@ GPIO.output(led_pin, GPIO.HIGH)
 #        print('OK Button Pressed')
 #        time.sleep(0.2)
 
-OUR_ADDRESS     = ('127.0.0.1', 3003) # listen to
-ADDRESS_TO_SEND = ('127.0.0.1', 3002) # send to
-sock = socket(AF_INET, SOCK_DGRAM)
-sock.setsockopt( SOL_SOCKET,SO_REUSEADDR, 1 )
-sock.bind( OUR_ADDRESS )
-sock.setsockopt( SOL_SOCKET,SO_BROADCAST, 1 )
+class KrpController:
+
+    baseUrl = "https://azfn-linqpad-web-app-e5opllzphjvwc.azurewebsites.net/api/krp"
+    minimumMinutesBetweenLogs = 0
+    users = []
+    latestLogByUserId = None #todo: get this from the server
+    selectedUserIndex = 0
+
+    returnTimer = None
+
+    def __init__(self, client):
+        self.client = client
+        self.client.controller = self
+
+    def init(self):
+        self.state = "LOADING"
+        self.client.init()
+        self.client.write(self.client.getIp(),"Loading...")
+        url = self.baseUrl + "/config"
+        response = requests.get(url=url)
+        data = response.json()
+        print(str(data))
+        self.minimumMinutesBetweenLogs = data['MinimumMinutesBetweenLogs']
+        for jUser in data['Users']:
+            self.users.append(KrpPlayer(jUser['Id'], jUser['FirstName'], jUser['LastName']))
+
+        self.client.clear()
+        self.__writeLastCleanedBy()
+        # self.client.loop()
+
+    def onButtonPress(self, button):            # handler for button press
+        if self.state == "MENU":
+            self.__selectPlayer()
+        elif self.state == "SELECT_PLAYER":
+            if button == "LEFT":
+                self.selectedUserIndex -= 1
+                if self.selectedUserIndex == -1: self.selectedUserIndex = len(self.users) - 1
+                self.__selectPlayer()
+            elif button == "RIGHT":
+                self.selectedUserIndex += 1
+                if self.selectedUserIndex == len(self.users): self.selectedUserIndex = 0
+                self.__selectPlayer()
+            elif button == "OK":
+                self.__registerPlayer(self.users[self.selectedUserIndex].id)
+        elif self.state == "ERROR":
+            self.__writeLastCleanedBy()
+
+    def __writeLastCleanedBy(self):             # Show main menu
+        self.state = "MENU"
+        user = self.__findUserById(self.latestLogByUserId)
+        self.client.write("Last cleaned by:", user.firstName if user is not None else "<NONE>")
+
+        self.__cancelReturningTimer()
+
+    def __selectPlayer(self):                   # Show select player on led
+        self.state = "SELECT_PLAYER"
+        user = self.users[self.selectedUserIndex]
+
+        self.__startReturningTimer()
+        
+        userString = str(user).ljust(14)
+        self.client.write("SELECT", "<" + userString + ">")
+
+    def __registerPlayer(self, userId):         # Show registing player on led
+        self.state = "REGISTERING"
+        self.client.write("Registering...", "")
+
+        self.__cancelReturningTimer()
+
+        try:
+            url = self.baseUrl + "/log?playerId=" + userId
+            requests.get(url=url)
+            self.latestLogByUserId = userId
+            self.__writeLastCleanedBy()
+        except:
+            self.__writeError("")
+            pass
+    
+    def __writeError(self, error):              # Show Error on Led when register failed
+        self.state = "ERROR"
+        self.client.write("Oops!", error)
+        self.__startReturningTimer()
+
+    def __findUserById(self, userId):           # Find user by userId
+        if userId is None:
+            return None
+        for user in self.users:
+            if user.id == userId:
+                return user
+        return None
+    
+    def __startReturningTimer(self):            # Timer for returning to main menu when no signal in 5 seconds.
+        self.__cancelReturningTimer()
+        self.returnTimer = Timer(5, self.__writeLastCleanedBy)
+        self.returnTimer.start()
+    
+    def __cancelReturningTimer(self):           # Cancel returing timer
+        if self.returnTimer != None and self.returnTimer.is_alive():
+            self.returnTimer.cancel()
+
+
+#################################################################
+
+class KrpPlayer:
+
+    def __init__(self, id, firstName, lastName):
+        self.id = id
+        self.firstName = firstName
+        self.lastName = lastName
+    
+    def __str__(self):
+        return self.firstName
+
+#################################################################
+
+class KrpClient:
+    controller = None
+
+    def init(self):
+        pass
+
+    def write(self,top16,bottom16):
+        pass
+
+    def clear(self):
+        self.write("","")
+
+    def loop(self):
+        pass
+
+    def getIp(self):
+        return gethostbyname(gethostname())
+
+class KrpConsoleClient(KrpClient):
+    lcd = None
+
+    def init(self, lcd):
+        self.lcd = lcd
+
+    def write(self, top16, bottom16):
+        top16 = '{0: <16}'.format(top16[:16])
+        bottom16 = '{0: <16}'.format(bottom16[:16])
+        self.lcd.message(data)
+        print("+----------------+")
+        print("|"+top16+"|")
+        print("+----------------+")
+        print("|"+bottom16+"|")
+        print("+----------------+")
+
+#################################################################
+
+controller = KrpController(KrpConsoleClient(lcd))
+controller.init()
 
 def buttonPressed(button):
-    sock.sendto(button.encode(), ADDRESS_TO_SEND)
+    controller.onButtonPress(button)
     print(button)
 
 GPIO.add_event_detect(left_button_pin, GPIO.FALLING, callback=lambda x: buttonPressed("LEFT"), bouncetime=2000)
@@ -76,14 +222,7 @@ GPIO.add_event_detect(right_button_pin, GPIO.FALLING, callback=lambda x: buttonP
 
 def run(*args):
     while True:
-        data, addr = sock.recvfrom(256)
-        data = data.decode("utf-8")
-        lcd.message(data)
-        print("+----------------+")
-        print("|"+data[:16]+"|")
-        print("+----------------+")
-        print("|"+data[16:]+"|")
-        print("+----------------+")
+        pass
 
 Thread(target=run).start()
 
