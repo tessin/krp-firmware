5892bd9f550f4790b237b4c14d73a1da40e3d547
Niels Bosma <niels@filestar.com>
2019-04-09 15:17:47 +0200

    Revert "Clean up."
    This reverts commit a7894f22d036d196d0f76e31897b6015db83b558.

--- krp.py
+++ krp.py
@@ -1,180 +1,85 @@
-import requests
-from socket import *
-from threading import Thread, Timer
+import Adafruit_CharLCD as LCD
+import RPi.GPIO as GPIO
+from time import sleep
+import commands
+from threading import Thread
 
-#################################################################
+GPIO.cleanup()
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(True)
 
-# Last cleaned by:
-# Niels
+lcd_rs = 25
+lcd_en = 24
+lcd_d4 = 23
+lcd_d5 = 17
+lcd_d6 = 18
+lcd_d7 = 22
+lcd_backlight = 4
+lcd_columns = 16
+lcd_rows = 2
 
-class KrpController:
+lcd = LCD.Adafruit_CharLCD(lcd_rs, lcd_en, lcd_d4, lcd_d5, lcd_d6, lcd_d7, lcd_columns, lcd_rows, lcd_backlight)
 
-    baseUrl = "https://azfn-linqpad-web-app-e5opllzphjvwc.azurewebsites.net/api/krp"
-    minimumMinutesBetweenLogs = 0
-    users = []
-    latestLogByUserId = None #todo: get this from the server
-    selectedUserIndex = 0
+lcd.home()
+lcd.clear()
 
-    returnTimer = None
+lcd.message(commands.getoutput('hostname -I'))
+sleep(5)
 
-    def __init__(self, client):
-        self.client = client
-        self.client.controller = self
+lcd.home()
+lcd.clear()
 
-    def init(self):
-        self.state = "LOADING"
-        self.client.init()
-        self.client.write(self.client.getIp(),"Loading...")
-        url = f"{self.baseUrl}/config"
-        response = requests.get(url=url)
-        data = response.json()
-        print(str(data))
-        self.minimumMinutesBetweenLogs = data['MinimumMinutesBetweenLogs']
-        for jUser in data['Users']:
-            self.users.append(KrpPlayer(jUser['Id'], jUser['FirstName'], jUser['LastName']))
+lcd.message("HelloWorld!\nFoo")
 
-        self.client.clear()
-        self.__writeLastCleanedBy()
-        # self.client.loop()
+led_pin = 20
+GPIO.setup(led_pin, GPIO.OUT)
 
-    def onButtonPress(self, button):            # handler for button press
-        if self.state == "MENU":
-            self.__selectPlayer()
-        elif self.state == "SELECT_PLAYER":
-            if button == "LEFT":
-                self.selectedUserIndex -= 1
-                if self.selectedUserIndex == -1: self.selectedUserIndex = len(self.users) - 1
-                self.__selectPlayer()
-            elif button == "RIGHT":
-                self.selectedUserIndex += 1
-                if self.selectedUserIndex == len(self.users): self.selectedUserIndex = 0
-                self.__selectPlayer()
-            elif button == "OK":
-                self.__registerPlayer(self.users[self.selectedUserIndex].id)
-        elif self.state == "ERROR":
-            self.__writeLastCleanedBy()
+left_button_pin = 13
+GPIO.setup(left_button_pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
-    def __writeLastCleanedBy(self):             # Show main menu
-        self.state = "MENU"
-        user = self.__findUserById(self.latestLogByUserId)
-        self.client.write("Last cleaned by:", user.firstName if user is not None else "<NONE>")
+ok_button_pin = 19
+GPIO.setup(ok_button_pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
-        self.__cancelReturningTimer()
+right_button_pin = 26
+GPIO.setup(right_button_pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
-    def __selectPlayer(self):                   # Show select player on led
-        self.state = "SELECT_PLAYER"
-        user = self.users[self.selectedUserIndex]
+# while True:
+#     GPIO.output(led_pin, GPIO.HIGH)
+#     print("hello")
+#     time.sleep(2)
+#     GPIO.output(led_pin, GPIO.LOW)
+#     time.sleep(2)
 
-        self.__startReturningTimer()
-        
-        userString = str(user).ljust(14)
-        self.client.write("SELECT", "<" + userString + ">")
+GPIO.output(led_pin, GPIO.HIGH)
 
-    def __registerPlayer(self, userId):         # Show registing player on led
-        self.state = "REGISTERING"
-        self.client.write("Registering...", "")
+#while True:
+#    input_state = GPIO.input(ok_button_pin)
+#    if input_state == False:
+#        print('OK Button Pressed')
+#        time.sleep(0.2)
 
-        self.__cancelReturningTimer()
+def buttonPressed(button):
+    print(button)
 
-        try:
-            url = f"{self.baseUrl}/log?playerId={userId}"
-            requests.get(url=url)
-            self.latestLogByUserId = userId
-            self.__writeLastCleanedBy()
-        except:
-            self.__writeError("")
-            pass
-    
-    def __writeError(self, error):              # Show Error on Led when register failed
-        self.state = "ERROR"
-        self.client.write("Oops!", error)
-        self.__startReturningTimer()
+GPIO.add_event_detect(left_button_pin, GPIO.FALLING, callback=lambda x: buttonPressed("left"), bouncetime=2000)
+GPIO.add_event_detect(ok_button_pin, GPIO.FALLING, callback=lambda x: buttonPressed("ok"), bouncetime=2000)
+GPIO.add_event_detect(right_button_pin, GPIO.FALLING, callback=lambda x: buttonPressed("right"), bouncetime=2000)
 
-    def __findUserById(self, userId):           # Find user by userId
-        if userId is None:
-            return None
-        for user in self.users:
-            if user.id == userId:
-                return user
-        return None
-    
-    def __startReturningTimer(self):            # Timer for returning to main menu when no signal in 5 seconds.
-        self.__cancelReturningTimer()
-        self.returnTimer = Timer(5, self.__writeLastCleanedBy)
-        self.returnTimer.start()
-    
-    def __cancelReturningTimer(self):           # Cancel returing timer
-        if self.returnTimer != None and self.returnTimer.is_alive():
-            self.returnTimer.cancel()
-
-
-#################################################################
-
-class KrpPlayer:
-
-    def __init__(self, id, firstName, lastName):
-        self.id = id
-        self.firstName = firstName
-        self.lastName = lastName
-    
-    def __str__(self):
-        return self.firstName
-
-#################################################################
-
-class KrpClient:
-    controller = None
-
-    def init(self):
-        pass
-
-    def write(self,top16,bottom16):
-        pass
-
-    def clear(self):
-        self.write("","")
-
-    def loop(self):
-        pass
-
-    def getIp(self):
-        return gethostbyname(gethostname())
-
-OUR_ADDRESS     = ('127.0.0.1', 3002) # listen to
-ADDRESS_TO_SEND = ('127.0.0.1', 3003) # send to
-
-class KrpConsoleClient(KrpClient):
-    sock = None
-
-    def init(self):
-        self.sock = socket(AF_INET, SOCK_DGRAM)
-        self.sock.setsockopt( SOL_SOCKET,SO_REUSEADDR, 1 )
-        self.sock.bind( OUR_ADDRESS )
-        self.sock.setsockopt( SOL_SOCKET,SO_BROADCAST, 1 )
-
-        def buttonHandler(*args):
-            while True:
-                try:
-                    data, addr = sock.recvfrom(256)
-                    button = data.decode("utf-8")
-                    print(button)
-                    controller.onButtonPress(button)
-                except:
-                    pass
-
-        Thread(target=buttonHandler).start()
-
-    def write(self, top16, bottom16):
-        top16 = '{0: <16}'.format(top16[:16])
-        bottom16 = '{0: <16}'.format(bottom16[:16])
-        self.sock.sendto((top16 + bottom16).encode(), ADDRESS_TO_SEND)
+def run(*args):
+    while True:
+        data, addr = sock.recvfrom(256)
+        data = data.decode("utf-8")
+        lcd.message(data)
         print("+----------------+")
-        print("|"+top16+"|")
+        print("|"+data[:16]+"|")
         print("+----------------+")
-        print("|"+bottom16+"|")
+        print("|"+data[16:]+"|")
         print("+----------------+")
 
-#################################################################
+Thread(target=run).start()
 
-controller = KrpController(KrpConsoleClient())
-controller.init()
+# try:
+#     while True:
+#         sleep(1)
+# except KeyboardInterrupt:
+#     GPIO.cleanup()
